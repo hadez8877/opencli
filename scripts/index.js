5e08828c8365a613d556329710296d0892a5c6eb
hadez8877 <hadezstreaming@gmail.com>
2026-06-11 19:44:12 -0500

    refactor(scripts): migrate from tsup to esbuild build system

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,10 @@
 export default async function run() {
 	const [cmd, ...args] = process.argv.slice(2);
 	switch (cmd) {
-		case 'sort-labels': {
-			const { default: sortLabels } = await import('./cmd/sort-labels.js');
-			await sortLabels('../../.github/labels.yml', ...args);
+		case 'dev':
+		case 'build': {
+			const { default: build } = await import('./cmd/build.js');
+			await build(...args, cmd === 'dev' ? 'IS_DEV' : undefined);
 			break;
 		}
 	}
